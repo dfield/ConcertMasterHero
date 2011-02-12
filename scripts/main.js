52421c204957343b9faf3ac38469139381e6995d
Charles <charles_yeh@brown.edu>
2011-02-12 01:03:43 -0500

    Changes merged.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 function draw(){
 	var paper = Raphael(0, 0, 500, 500);
 	var circle = paper.circle(125, 125, 75);
-	var rod = paper.rect(10, 10, 50, 50);s
+	var rod = paper.rect(10, 10, 50, 50);
 	circle.attr("stroke", "#fff");
 }
 
