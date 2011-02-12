a03ddb7c977f45432af73c454e81cc19ee98819f
unknown <Charles@.(none)>
2011-02-12 00:15:09 -0500

    Flash sound player added.

---
+++ scripts/flashCommunicator.js
@@ -0,0 +1,11 @@
+
+var flashMovie = getFlashMovie("synthesizer");
+
+function getFlashMovie(movieName) {
+	var isIE = navigator.appName.indexOf("Microsoft") != -1;
+	return (isIE) ? window[movieName] : document[movieName];
+}
+
+function playNote(msg){
+	flashMovie.playNote(msg);
+}
